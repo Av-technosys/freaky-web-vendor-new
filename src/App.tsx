42da4db889075efc572a545b670538499727a0e3
ashish11011 <bishnoi11011@gmail.com>
2025-11-06 12:45:21 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { IconArrowLeft } from "@tabler/icons-react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>
+        {" "}
+        <IconArrowLeft /> Test
+      </Button>
+    </>
+  );
+}
+
+export default App;
