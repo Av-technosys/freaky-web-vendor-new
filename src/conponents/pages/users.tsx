5adb04f0749ccc78e3340037ac8b93a28451eb96
Lavesh06 <kumarlavesh2001@gmail.com>
2025-12-01 11:49:33 +0530

    manage users

--- src/conponents/pages/users.tsx
+++ src/conponents/pages/users.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import img from "../../assets/testingProfilePicture.jpg"
+
 import { InputGroup, InputGroupAddon, InputGroupInput } from "../../components/ui/input-group";
 import { useState } from "react";
 import { Checkbox } from "../../components/ui/checkbox";
 import { Label, Separator } from "../../components/ui";
-import { IconMailFilled, IconPlus } from "@tabler/icons-react";
+import { IconMailFilled, IconPlus, IconTrash } from "@tabler/icons-react";
 import MultiselectorDialog from "../teamRoleMultiselectordialogbox";
 import MultipleSelector from "../../components/ui/multi-select";
-import DeleteMemberPopup from "../deleteMemberPopup";
+import ConfirmPopup from "../ConfirmPopup";
 
 
 const users = [
@@ -75,60 +76,17 @@ const Users = () => {
                     <p className="text-sm mt-2 text-gray-600">Manage your team members and their account permissions here.</p>
                     <Separator className="mt-4" />
                 </div>
-                <div className="flex w-full p-6">
-                    <div className="w-1/2">
+                <div className="lg:flex w-full p-6">
+                    <div className="lg:w-1/2 w-full">
                         <h1 className="text-3xl">Invite team members</h1>
                         <p className="text-sm mt-2 text-gray-600">Get your projects up and running faster by
                             inviting your team to collaborate.</p>
                     </div>
-
-
-                    {/* <div className="w-full space-y-4">
+                    <div className="w-full lg:mt-0 mt-10  space-y-4 text-black">
                         {fields.map((field, index) => (
-                            <InputGroup key={index}>
-                                <InputGroupInput
-                                    type="email"
-                                    placeholder="team@team.com"
-                                    value={field.email}
-                                    onChange={(e) => updateField(index, e.target.value)}
-                                />
-                                <InputGroupAddon>
+                            <div key={index} className="space-y-2 space-x-4  flex">
 
-                                    <IconMailFilled className="h-5 w-5 text-yellow-500" />
-                                </InputGroupAddon>
-                            </InputGroup>
-
-                        ))}
-
-                        <div className="flex justify-between mt-2">
-                            <div className="flex gap-2 items-center cursor-pointer">
-
-                                <IconPlus size={18} />
-                                <button
-                                    onClick={addField}
-                                    className=""
-                                >
-                                    Add another
-                                </button>
-                            </div>
-                            <div className="flex gap-2 items-center cursor-pointer">
-
-                                <IconMailFilled size={18} className="text-orange-500" />
-                                <button
-
-                                    className=" text-orange-500"
-                                >
-                                    Send Invites
-                                </button>
-                            </div>
-                        </div>
-                    </div> */}
-
-
-                    <div className="w-full space-y-4 text-black">
-                        {fields.map((field, index) => (
-                            <div key={index} className="space-y-2 flex">
-
+                                {/* Email Input */}
                                 <InputGroup>
                                     <InputGroupInput
                                         type="email"
@@ -141,20 +99,30 @@ const Users = () => {
                                     </InputGroupAddon>
                                 </InputGroup>
 
+                                {/* Role Selector */}
                                 <MultipleSelector
                                     commandProps={{ label: "Select Role" }}
                                     defaultOptions={categories}
                                     placeholder="Select Role"
-                                    // hideClearAllButton
-                                    // hidePlaceholderWhenSelected
                                     emptyIndicator={
                                         <p className="text-center text-black text-sm">No results found</p>
                                     }
                                     className="w-full text-black"
                                 />
+
+                                {/* DELETE BUTTON */}
+                                <button
+                                    onClick={() => {
+                                        const updated = fields.filter((_, i) => i !== index);
+                                        setFields(updated);
+                                    }}
+                                    className="text-red-500 hover:text-red-700"
+                                >
+                                    <IconTrash size={20} />
+                                </button>
+
                             </div>
                         ))}
-
                         <div className="flex justify-between mt-2">
                             <div className="flex gap-2 items-center cursor-pointer">
                                 <IconPlus size={18} />
@@ -172,15 +140,15 @@ const Users = () => {
                 </div>
                 <Separator className="mt-4 mb-4" />
 
-                <div className="flex w-full p-6">
-                    <div className="w-1/2">
+                <div className="lg:flex w-full p-6">
+                    <div className="lg:w-1/2 w-full">
                         <h1 className="text-3xl"> Team members</h1>
                         <p className="text-sm mt-2 text-gray-600">Get your projects up and running faster by
                             inviting your team to collaborate.</p>
                     </div>
 
 
-                    <div className="w-full ">
+                    <div className="w-full lg:mt-0 mt-10 text-black">
                         <div className="flex">
                             <Checkbox />
                             <Label className="ml-2">Name</Label>
@@ -189,22 +157,32 @@ const Users = () => {
                         <Separator className="mt-4" />
                         {users.map((user, index) => (
                             <div key={index}>
-                                <div className="flex justify-between w-full my-4">
+                                <div className="lg:flex grid grid-cols-2 justify-between w-full my-4">
                                     <div className="flex items-center gap-4 w-1/3">
                                         <Checkbox />
                                         <img src={user.img} alt={user.name} className="w-8 h-8 rounded-full" />
                                         <Label className="ml-2">{user.name}</Label>
                                     </div>
 
-                                    <div className="flex text-sm items-center justify-around w-2/3">
+                                    <div className="lg:flex grid  text-sm items-center justify-around w-2/3">
                                         <h1>{user.email}</h1>
                                         <h1>{user.phone}</h1>
-                                        <MultiselectorDialog>
+                                        <div className="flex gap-4"><MultiselectorDialog>
                                             <button className="text-orange-500">Edit</button>
                                         </MultiselectorDialog>
-                                        <DeleteMemberPopup>
-                                            <button className="text-red-500">Delete</button>
-                                        </DeleteMemberPopup>
+
+                                            <ConfirmPopup
+                                                title="Are you sure you want to delete this member?"
+                                                alertTitle="Delete Member"
+                                                description="This action cannot be undone. This will permanently delete the member from your team."
+                                                confirmText="Delete"
+                                                cancelText="Cancel"
+                                                onConfirm={() => {
+                                                    console.log("Member Deleted");
+                                                }}>
+                                                <button className="text-red-500">Delete</button>
+                                            </ConfirmPopup>
+                                        </div>
                                     </div>
 
                                 </div>
--- src/conponents/teamRoleMultiselectordialogbox.tsx
+++ src/conponents/teamRoleMultiselectordialogbox.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "../components/ui";
 import { Checkbox } from "../components/ui/checkbox";
 import {
     Dialog,
@@ -50,69 +51,15 @@ export default function MultiselectorDialog({ children }: { children: React.Reac
                 {children}
             </DialogTrigger>
 
-            <DialogContent className="lg:max-w-4xl rounded-4xl">
+            <DialogContent className="lg:max-w-2xl  rounded-4xl">
                 <DialogHeader>
-                    <DialogTitle className="text-3xl font-bold">Edit Roles</DialogTitle>
-                    {/* <DialogDescription className="mt-4">
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Admin</h1>
-                                <p>Admin manages operations and resources.</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Dashboard</h1>
-                                <p>Monitor, manage, and control system</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Manage Services</h1>
-                                <p>Organize, track, and modify services.</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Booking</h1>
-                                <p>View, manage, and track bookings</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Reviews</h1>
-                                <p>View and manage customer reviews</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Manage Bookings</h1>
-                                <p>Track, update, and control bookings</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="flex justify-between items-center mb-4">
-                            <div>
-                                <h1 className="text-black text-2xl font-bold">Company Profile</h1>
-                                <p>Manage your business profile information</p>
-                            </div>
-                            <div> <Checkbox /> </div>
-                        </div>
-                        <div className="float-right flex gap-2 text-xl font-bold">
-                            <button className="px-6 py-2 border-2 rounded-full border-primary shadow-2xl text-primary"> Cancel </button>
-                            <button className="px-6 py-2 border rounded-full border-primary shadow-2xl bg-primary text-white">Update</button>
-                        </div>
-                    </DialogDescription> */}
+                    <DialogTitle className="text-xl font-bold">Edit Roles</DialogTitle>
+
                     <DialogDescription className="mt-4">
                         {roles.map((role, index) => (
                             <div key={index} className="flex justify-between items-center mb-4">
                                 <div>
-                                    <h1 className="text-black text-2xl font-bold">{role.title}</h1>
+                                    <h1 className="text-black text-xl font-bold">{role.title}</h1>
                                     <p>{role.description}</p>
                                 </div>
                                 <div>
@@ -121,12 +68,14 @@ export default function MultiselectorDialog({ children }: { children: React.Reac
                             </div>
                         ))}
                         <div className="float-right flex gap-2 text-xl font-bold mt-4">
-                            <button className="px-6 py-2 border-2 rounded-full border-primary shadow-2xl text-primary">
+                            {/* <button className="px-6 py-2 border-2 rounded-full border-primary shadow-2xl text-primary">
                                 Cancel
                             </button>
                             <button className="px-6 py-2 border rounded-full border-primary shadow-2xl bg-primary text-white">
                                 Update
-                            </button>
+                            </button> */}
+                            <Button variant="outline">Cancel</Button>
+                            <Button >Update</Button>
                         </div>
                     </DialogDescription>
                 </DialogHeader>
