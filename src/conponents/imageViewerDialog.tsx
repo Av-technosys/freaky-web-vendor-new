c859c001fb71602cae90b3c2b0b939a8e6b13e40
ravindrasingh777 <ravindrasinghrss2004@gmail.com>
2026-01-16 18:32:56 +0530

    remove commented code!

--- src/conponents/imageViewerDialog.tsx
+++ src/conponents/imageViewerDialog.tsx
@@ -12,45 +12,6 @@ import {
 } from "../components/ui/alert-dialog";
 import { TiIconPlayerPlay } from "./icons";
 
-// const ImageViewerDialog = ({ mediaUrl, className = "" }: any) => {
-//   return (
-//     <>
-//       <AlertDialog>
-//         <AlertDialogTrigger asChild>
-//           <Button
-//             variant={"outline"}
-//             className={cn(
-//               " absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer h-7 w-7 hidden group-hover:flex",
-//               className
-//             )}
-//           >
-//             <TiIconPlayerPlay size="12" color="#D30000" />
-//           </Button>
-//         </AlertDialogTrigger>
-//         <AlertDialogContent className="md:max-w-4xl  ">
-//           <AlertDialogHeader>
-//             <AlertDialogDescription>
-//               <Alert className=" text-destructive border-none">
-//                 <AlertDescription>
-//                   <div className="w-full h-96">
-//                     <img
-//                       className="w-full h-full object-cover"
-//                       src={`${import.meta.env.VITE_IMAGE_BASE_URL}/${mediaUrl}`}
-//                       alt="uploaded"
-//                     />
-//                   </div>
-//                 </AlertDescription>
-//               </Alert>
-//             </AlertDialogDescription>
-//           </AlertDialogHeader>
-//           <AlertDialogFooter>
-//             <AlertDialogCancel>Cancel</AlertDialogCancel>
-//           </AlertDialogFooter>
-//         </AlertDialogContent>
-//       </AlertDialog>
-//     </>
-//   );
-// };
 
 const ImageViewerDialog = ({ mediaUrl, className = "" }: any) => {
   return (
